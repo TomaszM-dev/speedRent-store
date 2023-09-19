bf9d254b4caa61544b179d2d094e3fc59a33619a
TomaszM-dev <tomekmalocha@gmail.com>
2023-09-19 17:26:47 +0200

    creating navbar

---
+++ components/header.tsx
@@ -0,0 +1,173 @@
+"use client";
+
+import React from "react";
+import { useContext, useEffect, useState } from "react";
+
+import { Link } from "react-scroll";
+
+import { useMediaQuery } from "react-responsive";
+
+import { motion, AnimatePresence } from "framer-motion";
+import { BiMenuAltRight, BiX } from "react-icons/bi";
+
+// header component
+
+const Header = () => {
+  const [header, setHeader] = useState(false);
+  const [nav, setNav] = useState(false);
+
+  const desktopMode = useMediaQuery({
+    query: "(min-width: 1000px)",
+  });
+
+  // links
+  const linkData = [
+    {
+      name: "Home",
+
+      to: "home",
+      activeClass: "active",
+      spy: true,
+    },
+
+    {
+      name: "Products",
+
+      to: "products",
+      activeClass: "active",
+      spy: true,
+    },
+    {
+      name: "About",
+
+      to: "about",
+      activeClass: "active",
+      spy: true,
+    },
+    {
+      name: "Why us",
+
+      to: "why",
+      activeClass: "active",
+      spy: true,
+    },
+    {
+      name: "Testimonials",
+
+      to: "why",
+      activeClass: "active",
+      spy: true,
+    },
+    {
+      name: "Contact",
+
+      to: "contact",
+      activeClass: "active",
+      spy: true,
+    },
+    {
+      name: "",
+
+      to: "contact",
+      activeClass: "active",
+      spy: true,
+    },
+  ];
+
+  // adding background to navbar when scrolled
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 40) {
+        setHeader(true);
+      } else {
+        setHeader(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  return (
+    <header
+      className={`${
+        header ? " bg-white shadow-md py-2" : "bg-transparent shadow-none py-"
+      } fixed w-full max-w-[1900px] py-6 mx-auto px-10 z-20 transition-all duration-300 text-[1.1rem] `}
+    >
+      <div className="xl:container mx-auto flex  flex-col xl:flex-row xl:items-center xl:justify-between">
+        <div className="flex justify-between items-center px-4">
+          {/*  go back home */}
+          <Link
+            to="home"
+            smooth={desktopMode}
+            spy={true}
+            className={"cursor-pointer"}
+          >
+            <p className="text-[1.4rem] font-bold text-primary">
+              Speedy<span className="text-accent">Rental</span>
+            </p>
+          </Link>
+          {/* nav open menu */}
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer xl:hidden"
+          >
+            {nav ? (
+              <BiX className="text-4xl" />
+            ) : (
+              <BiMenuAltRight className="text-4xl" />
+            )}
+          </div>
+        </div>
+
+        {/* nav */}
+        <AnimatePresence>
+          {nav && (
+            <div className="max-h-max py-8 px-4  xl:px-0   gap-y-5 uppercase opacity-1 flex flex-col w-full  bg-white overflow-hidden font-bold xl:flex-row xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent xl:pb-0 transition-all duration-150 text-center">
+              {linkData.map((link) => (
+                <Link
+                  key={link.name}
+                  to={link.to}
+                  activeClass={link.activeClass}
+                  spy={link.spy}
+                  smooth={desktopMode}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          )}
+          {!nav && (
+            <div className="max-h-0 xl:max-h-max flex flex-col w-full  bg-white overflow-hidden font-bold xl:flex-row xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent xl:pb-0 transition-all duration-150 text-center">
+              {linkData.map((link) => (
+                <Link
+                  key={link.name}
+                  to={link.to}
+                  activeClass={link.activeClass}
+                  spy={link.spy}
+                  smooth={desktopMode}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          )}
+        </AnimatePresence>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+{
+  /* <nav
+className={`${
+  nav
+    ? "max-h-max py-8 px-4  xl:px-0   gap-y-5 uppercase opacity-1 "
+    : "max-h-0 xl:max-h-max  "
+} flex flex-col w-full  bg-white overflow-hidden font-bold xl:flex-row xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent xl:pb-0 transition-all duration-150 text-center `}
+> */
+}
